b4eb8bdbfaa8809ff7e49d102056e77a70e1aa62
Marikit Yake <marikitc@outlook.com>
2023-04-17 10:04:22 -0600

    draw n-dimensional polygon to the screen

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,31 @@
 // https://github.com/processing/p5.js/wiki/Global-and-instance-mode
 import p5 from "p5";
 
-/**
- * @param {p5} s
- */
+/*
+** @param {p5} s
+*/
+
 const sketch = function (s) {
+  polygon = function (x, y, radius, npoints) {
+      let angle = s.TWO_PI / npoints;
+      s.beginShape();
+      for (let a = 0; a < s.TWO_PI; a += angle) {
+        let sx = x + Math.cos(a) * radius;
+        let sy = y + Math.sin(a) * radius;
+        s.vertex(sx, sy);
+      }
+      s.endShape(s.CLOSE);
+    };
+
     s.setup = function () {
         s.createCanvas(s.windowWidth, s.windowHeight);
     };
 
     s.draw = function () {
-        s.background(255);
-        s.fill(0);
-        s.ellipse(s.windowWidth / 2, s.windowHeight / 2, 50, 50);
+        s.background("#FEFBEA");
+        s.fill("#E62B09");
+        polygon(s.windowWidth / 2, s.windowHeight / 2, 120, 6);
+    
     };
 };
 
